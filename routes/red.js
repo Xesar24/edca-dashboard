8c9d098306c30bc4dee5f6b4a37b990a7cdef2fd
Mario Torres <mario291187@gmail.com>
2017-11-09 12:28:10 -0600

    Adding data/aff.json

--- routes/red.js
+++ routes/red.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('red', { title: 'Contrataciones Abiertas' });
+    res.render('red_compartida/index', { title: 'Contrataciones Abiertas' });
 });
 
 module.exports = router;
